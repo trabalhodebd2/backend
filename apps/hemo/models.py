f3506219bf3518c3b4ac98cbe8b1331f25fd8f49
ivesfg1 <wigzfurtado@gmail.com>
2022-11-20 20:25:21 -0300

    adicionando cidade e uf

--- apps/hemo/models.py
+++ apps/hemo/models.py
@@ -7,3 +7,9 @@ from ..base_app.models import BaseModel
 class Hemo(BaseModel):
     title = gis_models.CharField(max_length=255, verbose_name="Nome")
     geometry = gis_models.PointField(verbose_name="Latitude")
+    city = gis_models.CharField(
+        max_length=255, verbose_name="Cidade", blank=True, null=True
+    )
+    uf = gis_models.CharField(
+        max_length=2, verbose_name="Estado", blank=True, null=True
+    )
